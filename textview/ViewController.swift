604dec65aeff5cd4dc98425e37905a401fb765c7
Subash <subash_chandra@apple.com>
2016-09-13 09:38:44 -0700

    first commit

--- textview/ViewController.swift
+++ textview/ViewController.swift
@@ -1,8 +1,3 @@
-//
-//  ViewController.swift
-//  textview
-//
-//  Created by Subash Dantuluri on 4/28/16.
 //  Copyright © 2016 Subash Dantuluri. All rights reserved.
 //
 
@@ -10,16 +5,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var texView: UITextView!
+    @IBOutlet weak var texField: UITextView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      super.viewDidLoad()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func submit(sender: AnyObject) {
+        let message = texField.text.stringByReplacingOccurrencesOfString("\n", withString:"<br>")
+        
+        do {
+            texView?.attributedText = message.dataUsingEncoding(NSUTF8StringEncoding).flatMap { data -> NSMutableAttributedString? in
+                guard let string = try? NSMutableAttributedString(data: data, options: [
+                    NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType,
+                    NSCharacterEncodingDocumentAttribute : NSUTF8StringEncoding
+                    ], documentAttributes: nil) else { return nil }
+                return string
+            }
+        }
+        
     }
-
-
 }
 
+
+//Try strings like
+// "<i>Thank you for waiting.</i>"
+// "<u>Thank you for waiting.</u>"
+// "<b>Thank you for waiting.</b>"
